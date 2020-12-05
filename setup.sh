797db52237c061956b5140e6ab09a9fc3b9723e7
nazunamoe <nakashimayuki@kakao.com>
2020-12-05 22:22:33 +0900

    setup: Switch to proton-clang

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ export ARCH=arm64
 export SUBARCH=arm64
 export DTC_EXT=dtc
 
+PATH="/home/nazunamoe/proton-clang/bin:${PATH}" \
+# PATH="/home/nazunamoe/clang/bin:/home/nazunamoe/aarch64-linux-android-4.9/bin:/home/nazunamoe/arm-linux-androideabi-4.9/bin:${PATH}" \
 
-# PATH="/home/nazunamoe/proton-clang/bin:${PATH}" \
-PATH="/home/nazunamoe/clang/bin:/home/nazunamoe/aarch64-linux-android-4.9/bin:/home/nazunamoe/arm-linux-androideabi-4.9/bin:${PATH}" \
-
-# make -j$(nproc --all) O=out \
+#make -j$(nproc --all) O=out \
 #			ARCH=arm64 \
 #			CC=clang \
 #			AR=llvm-ar \
@@ -34,7 +33,7 @@ rm -f /home/nazunamoe/anykernel2/*.dtbo
 
 cp out/arch/arm64/boot/Image.gz-dtb /home/nazunamoe/anykernel2/Image.gz-dtb
 
-python /home/nazunamoe/lib/src/mkdtboimg.py create /home/nazunamoe/anykernel2/dtbo.img /home/nazunamoe/test_kernel/out/arch/arm64/boot/dts/qcom/*.dtbo
+python /home/nazunamoe/lib/src/mkdtboimg.py create /home/nazunamoe/anykernel2/dtbo.img /home/nazunamoe/kernel_temp/out/arch/arm64/boot/dts/qcom/*.dtbo
 
 cd /home/nazunamoe/anykernel2
 
@@ -42,4 +41,4 @@ FORMAT="%Y%m%d-%H%M"
 _DATE=$(date +"$FORMAT" )
 
 zip -r "Nanahira_Kernel_${_DATE}.zip" * --exclude *.zip
-cd /home/nazunamoe/test_kernel
+cd /home/nazunamoe/kernel_temp
