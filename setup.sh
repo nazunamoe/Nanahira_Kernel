1204da8e1b1ea8a0d6c928e138368a2b2a7a4045
nazunamoe <nakashimayuki@kakao.com>
2020-12-13 02:18:39 +0900

    Proton clang bringup

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ export ARCH=arm64
 export SUBARCH=arm64
 export DTC_EXT=dtc
 
-PATH="/home/nazunamoe/proton-clang/bin:${PATH}" \
-# PATH="/home/nazunamoe/clang/bin:/home/nazunamoe/aarch64-linux-android-4.9/bin:/home/nazunamoe/arm-linux-androideabi-4.9/bin:${PATH}" \
+# PATH="/home/nazunamoe/proton-clang/bin:${PATH}" \
+PATH="/home/nazunamoe/proton-clang/bin:/home/nazunamoe/aarch64-linux-android-4.9/bin:/home/nazunamoe/arm-linux-androideabi-4.9/bin:${PATH}" \
 
 #make -j$(nproc --all) O=out \
 #			ARCH=arm64 \
@@ -24,6 +24,10 @@ make -j$(nproc --all) O=out \
 			ARCH=arm64 \
 			CC=clang \
 			CLANG_TRIPLE=aarch64-linux-gnu- \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			STRIP=llvm-strip \
 			CROSS_COMPILE=aarch64-linux-android- \
 			CROSS_COMPILE_ARM32=arm-linux-androideabi-
                      
@@ -33,7 +37,7 @@ rm -f /home/nazunamoe/anykernel2/*.dtbo
 
 cp out/arch/arm64/boot/Image.gz-dtb /home/nazunamoe/anykernel2/Image.gz-dtb
 
-python /home/nazunamoe/lib/src/mkdtboimg.py create /home/nazunamoe/anykernel2/dtbo.img /home/nazunamoe/kernel_temp/out/arch/arm64/boot/dts/qcom/*.dtbo
+python /home/nazunamoe/lib/src/mkdtboimg.py create /home/nazunamoe/anykernel2/dtbo.img /home/nazunamoe/test_kernel/out/arch/arm64/boot/dts/qcom/*.dtbo
 
 cd /home/nazunamoe/anykernel2
 
